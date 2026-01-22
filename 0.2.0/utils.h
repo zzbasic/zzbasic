25e68aa56d766d911075263d932358efb9756d58
arataca89 <arataca89@gmail.com>
2026-01-22 16:29:38 -0300

    Add files via upload

--- 0.2.0/utils.h
+++ 0.2.0/utils.h
@@ -1,10 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define BUFFER_SIZE       256    // Para mensagens de erro LONGAS
-#define TOKEN_TEXT_SIZE   128    // Para texto de token (números, operadores)
-#define MAX_NUMBER_LEN    63     // Específico do lexer
-
 void setup_utf8();
 
 void wait();
---
+++ 0.2.0/zzdefs.h
@@ -0,0 +1,10 @@
+#ifndef ZZ_DEFS_H
+#define ZZ_DEFS_H
+
+#define BUFFER_SIZE       	256    // Para mensagens de erro LONGAS
+#define MAX_NUMBER_LENGTH 	63
+#define MAX_NUMBER_LEN 		63 
+#define MAX_IDENTIFIER_LEN	63
+#define TOKEN_TEXT_SIZE   	128    // Para texto de token (números, operadores)
+
+#endif
