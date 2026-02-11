8f39e13b98f0a813b52229ab1577347e2f392ac8
arataca89 <arataca89@gmail.com>
2026-02-11 05:37:03 -0300

    Add files via upload

--- 0.6.0/zzdefs.h
+++ 0.6.0/zzdefs.h
@@ -8,15 +8,14 @@
 
 // TAMANHO MAXIMO
 #define BUFFER_SIZE       	256    // Para mensagens de erro LONGAS
-#define NUMBER_SIZE 		64
-#define VARNAME_SIZE 		64
-#define TOKENTEXT_SIZE   	128    // Para texto de token (números, operadores)
-#define STRING_SIZE 		256
-
+#define NUMBER_SIZE 		24
+#define VARNAME_SIZE 		32
+#define TOKENTEXT_SIZE   	64    // Para texto de token (números, operadores)
+#define STRING_SIZE 		128
+#define PROGRAM_LINE_SIZE	80
 
 // REPL Multi-line Program Storage
-#define MAX_PROGRAM_LINES 100
-#define PROGRAM_LINE_SIZE BUFFER_SIZE
+#define MAX_PROGRAM_LINES	50
 
 #endif
 // Fim de zzdefs.h
