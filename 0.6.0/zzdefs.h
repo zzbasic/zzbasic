28b864f771d846b5b1f52e73ff6fe19b5efc5f47
arataca89 <arataca89@gmail.com>
2026-02-17 18:22:35 -0300

    Upload automático: 0.6.0/zzdefs.h

--- 0.6.0/zzdefs.h
+++ 0.6.0/zzdefs.h
@@ -13,5 +13,10 @@
 // REPL Multi-line 
 #define MAX_PROGRAM_LINES	50
 
+// Tamanhos de buffers locais/temporários
+#define ERROR_MSG_SIZE      256    // Para mensagens de erro locais
+#define TEMP_BUFFER_SIZE    512    // Para buffers temporários
+#define FORMAT_BUFFER_SIZE  1024   // Para formatação de saída
+
 #endif
 // Fim de zzdefs.h
