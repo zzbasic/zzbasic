e0a53ce9904facdb3895de91b20da6c720695a03
arataca89 <arataca89@gmail.com>
2026-02-17 06:55:18 -0300

    Upload automático: 0.6.0/zzdefs.h

--- 0.6.0/zzdefs.h
+++ 0.6.0/zzdefs.h
@@ -7,19 +7,11 @@
 #define ZZ_PROMPT ">> "
 
 // TAMANHO MAXIMO
-#define BUFFER_SIZE       	256    // Para mensagens de erro LONGAS
 #define NUMBER_SIZE 		24
 #define VARNAME_SIZE 		32
-#define TOKENTEXT_SIZE   	64    // Para texto de token (números, operadores)
-#define STRING_SIZE 		128
-#define PROGRAM_LINE_SIZE	80
 
 // REPL Multi-line 
 #define MAX_PROGRAM_LINES	50
-
-// TIPO TEXT
-#define TEXT_INITIAL_CAPACITY 	64 	// Tamanho inicial (64 bytes)
-#define TEXT_GROWTH_FACTOR 		1.5 // Fator de crescimento (1.5x)
 
 #endif
 // Fim de zzdefs.h
