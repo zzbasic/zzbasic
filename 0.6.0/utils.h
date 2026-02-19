8914f5d8403eb92c388755199a28073cf6f0e64b
arataca89 <arataca89@gmail.com>
2026-02-19 17:02:56 -0300

    Upload automático: 0.6.0/utils.h

--- 0.6.0/utils.h
+++ 0.6.0/utils.h
@@ -6,5 +6,20 @@ void setup_utf8();
 
 void wait();
 
+/********************************************************************
+Exibe uma linha de código-fonte com uma seta apontando para o erro
+
+Parâmetros:
+	* source      Ponteiro para o código-fonte completo
+	* source_size Tamanho total do código-fonte em bytes
+	* line        Número da linha (1-indexado)
+	* column      Número da coluna (1-indexado)
+ 
+Exemplo de saída:
+	Linha 5: let x = 10 + 
+                      ↑
+*********************************************************************/
+void display_error_location(const char* source, int source_size, int line, int column);
+
 #endif
 // Fim de utils.h
