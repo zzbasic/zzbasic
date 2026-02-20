1f8a840487227e1735aef96ec3032a5d39ff49cb
arataca89 <arataca89@gmail.com>
2026-02-20 06:47:37 -0300

    Upload automático: color_mapping.h

---
+++ color_mapping.h
@@ -0,0 +1,19 @@
+// color_mapping.h - MAPEAMENTO ENTRE TOKEN_TYPE E COLOR_CODE
+
+#ifndef COLOR_MAPPING_H
+#define COLOR_MAPPING_H
+
+#include "lexer.h"       		// Para TokenType
+#include "color_constants.h" 	// Para ColorCode  
+
+// Função para converter TokenType para ColorCode
+ColorCode token_to_color_code(TokenType token);
+
+// Função para obter sequência ANSI a partir de ColorCode
+const char* color_code_to_ansi(ColorCode  code);
+
+// Função para obter sequência ANSI diretamente de TokenType
+const char* token_to_ansi(TokenType token);
+
+#endif // COLOR_MAPPING_H
+// Fim de color_mapping.h
