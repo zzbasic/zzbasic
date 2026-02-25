cece67f34d343a24b3e2c2e4f3490838a0b584ee
arataca89 <arataca89@gmail.com>
2026-02-25 16:38:14 -0300

    Upload automático: 0.7.0/scope.h

--- 0.7.0/scope.h
+++ 0.7.0/scope.h
@@ -17,6 +17,8 @@ typedef struct {
     int scope_level;           // Contador de profundidade
 } ScopeStack;
 
+int symbol_exists(SymbolTable* table, const char* name);
+
 ScopeStack* scope_stack_create(void);
 void scope_stack_destroy(ScopeStack* scope_stack);
 void scope_push(ScopeStack* scope_stack);
@@ -25,6 +27,13 @@ Scope* scope_get_current(ScopeStack* scope_stack);
 SymbolTable* scope_get_symbols(ScopeStack* scope_stack);
 Symbol* scope_lookup(ScopeStack* scope_stack, const char* name);
 int scope_set_value(ScopeStack* scope_stack, const char* name, EvaluatorResult value);
+
+// Funções de leitura que fazem lookup em todos os escopos
+int scope_get_number(ScopeStack* stack, const char* name, double* out_value);
+int scope_get_string(ScopeStack* stack, const char* name, char* out_value, size_t max_len);
+int scope_get_bool(ScopeStack* stack, const char* name, int* out_value);
+int scope_get_text(ScopeStack* stack, const char* name, Text** out_text);
+int scope_get_array(ScopeStack* stack, const char* name, Array** out_array);
 
 #endif
 // Fim de scope.h
